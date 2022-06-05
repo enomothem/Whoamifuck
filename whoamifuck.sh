13449d05c2513d4b79bb45b08c657b218b2a6f3d
Enomothem <45089051+enomothem@users.noreply.github.com>
2022-06-05 19:50:27 +0800

    Update whoamifuck.sh

--- whoamifuck.sh
+++ whoamifuck.sh
@@ -29,6 +29,52 @@ SUDO=`more /etc/sudoers | grep -v "^#|^$" | grep "ALL=(ALL)"`
 #FW=`service --status-all`
 #JC=`ps aux`
 
+# [ ++ Function ++ ]
+
+function user()
+{
+        echo
+        T='11'
+
+        LOG=/tmp/valid.$$.log
+        grep -v "invalid" $AUTHLOG > $LOG
+        users=$(grep "Failed password" $LOG | awk '{ print $(NF-5) }' | sort | uniq)
+        printf "\e[4;34m%-5s|%-10s|%-15s|%-19s|%-33s|%s\n\e[0m" "Sr#" "登入用户名" "尝试次数" "IP地址" "虚拟主机映射" "时间范围"
+        ucount=0;
+        ip_list="$(egrep -o "[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+" $LOG | sort | uniq)"
+        for ip in $ip_list;
+        do
+                grep $ip $LOG > /tmp/temp.$$.log
+        for user in $users;
+        do
+                grep $user /tmp/temp.$$.log> /tmp/$$.log
+                cut -c-16 /tmp/$$.log > $$.time
+                tstart=$(head -1 $$.time);
+                start=$(date -d "$tstart" "+%s");
+                tend=$(tail -1 $$.time);
+                end=$(date -d "$tend" "+%s")
+
+                limit=$(( $end - $start ))
+
+                if [ $limit -gt 120 ];
+                then
+                        let ucount++;
+
+                        IP=$(egrep -o "[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+" /tmp/$$.log | head -1 );
+                        TIME_RANGE="$tstart-->$tend"
+
+                        ATTEMPTS=$(cat /tmp/$$.log|wc -l);
+
+                        HOST=$(host $IP | awk '{ print $NF }' )
+
+                printf "%-5s|%-10s|%-11s|%-17s|%-27s|%-s\n" "$ucount" "$user" "$ATTEMPTS" "$IP" "$HOST" "$TIME_RANGE";
+                fi
+        done
+        done
+
+        rm /tmp/valid.$$.log/tmp/$$.log $$.time/tmp/temp.$$.log 2>/dev/null
+        rm *.time
+}
 
 
 # [ ++ LOGO ++ ]
@@ -60,50 +106,7 @@ case ${op} in
         -f | --file) FILE="${2}"
                 echo "你使用的文件是$FILE"
                 printf "\e[1;31m                    [\t用户登入信息\t]                                    \e[0m\n"
-                echo
-
-
-
-                T='11'
-
-                LOG=/tmp/valid.$$.log
-                grep -v "invalid" $AUTHLOG > $LOG
-                users=$(grep "Failed password" $LOG | awk '{ print $(NF-5) }' | sort | uniq)
-                printf "\e[4;34m%-5s|%-10s|%-15s|%-19s|%-33s|%s\n\e[0m" "Sr#" "登入用户名" "尝试次数" "IP地址" "虚拟主机映射" "时间范围"
-                ucount=0;
-                ip_list="$(egrep -o "[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+" $LOG | sort | uniq)"
-                for ip in $ip_list;
-                do
-                        grep $ip $LOG > /tmp/temp.$$.log
-                for user in $users;
-                do
-                        grep $user /tmp/temp.$$.log> /tmp/$$.log
-                        cut -c-16 /tmp/$$.log > $$.time
-                        tstart=$(head -1 $$.time);
-                        start=$(date -d "$tstart" "+%s");
-                        tend=$(tail -1 $$.time);
-                        end=$(date -d "$tend" "+%s")
-
-                        limit=$(( $end - $start ))
-
-                        if [ $limit -gt 120 ];
-                        then
-                                let ucount++;
-
-                                IP=$(egrep -o "[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+" /tmp/$$.log | head -1 );
-                                TIME_RANGE="$tstart-->$tend"
-
-                                ATTEMPTS=$(cat /tmp/$$.log|wc -l);
-
-                                HOST=$(host $IP | awk '{ print $NF }' )
-
-                        printf "%-5s|%-10s|%-11s|%-17s|%-27s|%-s\n" "$ucount" "$user" "$ATTEMPTS" "$IP" "$HOST" "$TIME_RANGE";
-                        fi
-                done
-                done
-
-                rm /tmp/valid.$$.log/tmp/$$.log $$.time/tmp/temp.$$.log 2>/dev/null
-                rm *.time
+                user
                 ;;
         -u | --user-device)     
                 printf "\e[1;31m                    [\t用户基本信息\t]                                    \e[0m\n"
@@ -178,50 +181,7 @@ case ${op} in
                 echo
 
                 printf "\e[1;31m                    [\t用户登入信息\t]                                    \e[0m\n"
-                echo
-
-
-
-                T='11'
-
-                LOG=/tmp/valid.$$.log
-                grep -v "invalid" $AUTHLOG > $LOG
-                users=$(grep "Failed password" $LOG | awk '{ print $(NF-5) }' | sort | uniq)
-                printf "\e[4;34m%-5s|%-10s|%-15s|%-19s|%-33s|%s\n\e[0m" "Sr#" "登入用户名" "尝试次数" "IP地址" "虚拟主机映射" "时间范围"
-                ucount=0;
-                ip_list="$(egrep -o "[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+" $LOG | sort | uniq)"
-                for ip in $ip_list;
-                do
-                        grep $ip $LOG > /tmp/temp.$$.log
-                for user in $users;
-                do
-                        grep $user /tmp/temp.$$.log> /tmp/$$.log
-                        cut -c-16 /tmp/$$.log > $$.time
-                        tstart=$(head -1 $$.time);
-                        start=$(date -d "$tstart" "+%s");
-                        tend=$(tail -1 $$.time);
-                        end=$(date -d "$tend" "+%s")
-
-                        limit=$(( $end - $start ))
-
-                        if [ $limit -gt 120 ];
-                        then
-                                let ucount++;
-
-                                IP=$(egrep -o "[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+" /tmp/$$.log | head -1 );
-                                TIME_RANGE="$tstart-->$tend"
-
-                                ATTEMPTS=$(cat /tmp/$$.log|wc -l);
-
-                                HOST=$(host $IP | awk '{ print $NF }' )
-
-                        printf "%-5s|%-10s|%-11s|%-17s|%-27s|%-s\n" "$ucount" "$user" "$ATTEMPTS" "$IP" "$HOST" "$TIME_RANGE";
-                        fi
-                done
-                done
-
-                rm /tmp/valid.$$.log/tmp/$$.log $$.time/tmp/temp.$$.log 2>/dev/null
-                rm *.time
+                user
                 ;;
         *)
                 printf "usage:  \n\n"
